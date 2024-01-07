b835e9ce6b9aa397b45cda85046d1465732797bc
AST-LW <walkthroughfrom2020@gmail.com>
2024-01-08 02:40:44 +0530

    category screen small update nsvigation to product display screen

--- src/components/app/CategoriesScreen.js
+++ src/components/app/CategoriesScreen.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native";
+import newArrivals from "../../../assets/data/new-arrivals.json";
+import { useNavigation } from "@react-navigation/native";
+import { LOADING_SCREEN, PRODUCT_DISPLAY_SCREEN } from "../../../constants/screens";
+import { useDispatch } from "react-redux";
+import { hideTabBar } from "../../store/slices/appUIStateSlice";
 
-const CategoryCard = ({ imageUrl, title }) => {
+const CategoryCard = ({ imageUrl, title, products }) => {
+    const navigation = useNavigation();
+    const dispatch = useDispatch();
+    const filteredProducts = products.filter((product) => product.category === title.toLowerCase());
     return (
-        <TouchableOpacity>
+        <TouchableOpacity
+            onPress={() => {
+                dispatch(hideTabBar());
+                navigation.navigate(LOADING_SCREEN, {
+                    navigateTo: PRODUCT_DISPLAY_SCREEN,
+                    products: filteredProducts,
+                });
+            }}
+        >
             <View className="ml-1 mr-1">
                 <Image source={imageUrl} className="h-24 w-28 rounded object-cover" />
             </View>
@@ -13,6 +29,7 @@ const CategoryCard = ({ imageUrl, title }) => {
 };
 
 const CategoryScreen = () => {
+    const allProducts = [...newArrivals.products];
     return (
         <ScrollView
             className
@@ -23,15 +40,36 @@ const CategoryScreen = () => {
             horizontal
             showsHorizontalScrollIndicator={false}
         >
-            <CategoryCard imageUrl={require("../../../assets/images/categories/clothing.jpg")} title={"Clothing"} />
-            <CategoryCard imageUrl={require("../../../assets/images/categories/shoes.jpg")} title={"Shoes"} />
-            <CategoryCard imageUrl={require("../../../assets/images/categories/furniture.jpg")} title={"Furniture"} />
-            <CategoryCard imageUrl={require("../../../assets/images/categories/toys.jpg")} title={"Toys"} />
+            <CategoryCard
+                imageUrl={require("../../../assets/images/categories/clothing.jpg")}
+                title={"Clothing"}
+                products={allProducts}
+            />
+            <CategoryCard
+                imageUrl={require("../../../assets/images/categories/shoes.jpg")}
+                title={"Shoes"}
+                products={allProducts}
+            />
+            <CategoryCard
+                imageUrl={require("../../../assets/images/categories/furniture.jpg")}
+                title={"Furniture"}
+                products={allProducts}
+            />
+            <CategoryCard
+                imageUrl={require("../../../assets/images/categories/toys.jpg")}
+                title={"Toys"}
+                products={allProducts}
+            />
             <CategoryCard
                 imageUrl={require("../../../assets/images/categories/audio-equipment.jpg")}
                 title={"Audio sets"}
+                products={allProducts}
             />
-            <CategoryCard imageUrl={require("../../../assets/images/categories/books.jpg")} title={"Books"} />
+            <CategoryCard
+                imageUrl={require("../../../assets/images/categories/books.jpg")}
+                title={"Books"}
+                products={allProducts}
+            />
         </ScrollView>
     );
 };
