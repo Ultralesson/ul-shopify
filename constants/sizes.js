8b27b7c703488f2eef8714d97cb3b95be0d42873
AST-LW <walkthroughfrom2020@gmail.com>
2024-01-12 03:03:31 +0530

    gif-videos

--- constants/sizes.js
+++
@@ -1,4 +0,0 @@
-export const ICON_SIZE_LARGE = 30;
-export const ICON_SIZE_MEDIUM = 25;
-export const ICON_SIZE_SMALL = 20;
-export const ICON_ULTRA_SMALL = 15;
--- src/modals/Modals.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import OtpQuitModal from "./OtpQuitModal";
-
-const Modals = () => {
-    return <OtpQuitModal />;
-};
-
-export default Modals;
